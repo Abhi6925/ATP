59667162ba36e696159b492ac743921c358bf033
Abhi6925 <abhijithreddy321@gmail.com>
2026-03-12 21:20:44 +0530

    Delete Week2 directory

--- Week2/restParameter.js
+++
@@ -1,9 +0,0 @@
-function sum(...a){
-    let sum=0
-    for(let element of a){
-        sum+=element
-    }
-    console.log(sum)
-}
-
-sum(1,2,3,4,3,2,2,2)
--- Week2/spreadOp2.js
+++
@@ -1,9 +0,0 @@
-// Exercise 2: Update User Object
-let user = {
-    name: "Ravi",
-    city: "Hyderabad"
-};
-
-let updateUser={...user,age:25}
-console.log(user)
-console.log(updateUser)
